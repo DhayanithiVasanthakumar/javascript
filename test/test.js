f6d95af7c7e28f7dadde0df46dca39f14ccf3eac
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2025-02-26 22:22:40 +0530

    function example

--- test/test.js
+++ test/test.js
@@ -1,7 +1,14 @@
-var num=prompt("Enter a number")
-if(num%2==0){
-    console.log("The number is even");
-    alert("even")
-}else
-console.log("it is odd number")
-alert("odd")
+// var num=prompt("Enter a number")
+// if(num%2==0){
+//     console.log("The number is even");
+//     alert("even")
+// }else
+// console.log("it is odd number")
+// alert("odd")
+
+//functions
+function greet(name) {
+    console.log("Hello, " + name);
+}
+names=prompt("enter name")
+greet(names)
